757ea32ac2ec095fc08583a2a61db731a45800a6
Hien <minhhien1062@gmail.com>
2026-05-05 15:23:07 +0700

    task manger

---
+++ src/components/tasks/TaskForm.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+
+const TaskForm = ({ onAdd }) => {
+	const [title, setTitle] = useState("");
+	const [priority, setPriority] = useState("medium");
+	const [deadline, setDeadline] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (onAdd) {
+			onAdd({
+				title,
+				priority,
+				deadline,
+        status: "todo"
+			});
+		}
+
+    //reset
+    setTitle("")
+    setPriority("medium")
+    setDeadline("")
+	};
+
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className="border border-slate-200 p-4 rounded-xl bg-white shadow-sm flex flex-wrap gap-4 items-end"
+		>
+			<div className="flex-1 min-w-[250px]">
+				<label className="block text-xs font-semibold text-slate-500 mb-1.5 uppercase tracking-wide">
+					Task Title *
+				</label>
+				<input
+					className="w-full border border-slate-200 px-3 py-2 rounded-lg outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 transition-all text-sm"
+					type="text"
+					placeholder="What needs to be done?"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+			</div>
+
+			<div className="w-[140px]">
+				<label className="block text-xs font-semibold text-slate-500 mb-1.5 uppercase tracking-wide">
+					Priority
+				</label>
+				<select
+					value={priority}
+					onChange={(e) => setPriority(e.target.value)}
+					className="w-full border border-slate-200 px-3 py-2 rounded-lg outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 transition-all text-sm bg-white cursor-pointer"
+				>
+					<option value="high">🔴 High</option>
+					<option value="medium">🟡 Medium</option>
+					<option value="low">🔵 Low</option>
+				</select>
+			</div>
+
+			<div className="w-[160px]">
+				<label className="block text-xs font-semibold text-slate-500 mb-1.5 uppercase tracking-wide">
+					Deadline
+				</label>
+				<input
+					type="date"
+					value={deadline}
+					onChange={(e) => setDeadline(e.target.value)}
+					className="w-full border border-slate-200 px-3 py-2 rounded-lg outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 transition-all text-sm cursor-pointer"
+				/>
+			</div>
+
+			{/* Nút Submit */}
+			<button
+				type="submit"
+				className="bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-2 px-6 rounded-lg transition-colors h-[38px] text-sm cursor-pointer shadow-sm shadow-indigo-200"
+			>
+				Add Task
+			</button>
+		</form>
+	);
+};
+
+export default TaskForm;
